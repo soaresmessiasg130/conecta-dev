812339d985ed6dd73d841d8c588a18ba719b081b
soaresmessiasg130 <soaresmessiasg130@gmail.com>
2020-07-04 17:30:26 -0300

    Theme Adjusts >>> Colors Main, Dark And Light Seted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Home from "./pages/Home";
+import { ThemeProvider } from '@material-ui/core/styles';
+
+import theme from './theme';
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>
+        <Home />
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme/palette.js
@@ -0,0 +1,14 @@
+import { colors } from '@material-ui/core';
+
+export default {
+  primary: {
+    light: colors.lightGreen[500],
+    main: colors.lightGreen[700],
+    dark: colors.lightGreen[900],
+  },
+  secondary: {
+    light: colors.blueGrey[500],
+    main: colors.blueGrey[700],
+    dark: colors.blueGrey[900],
+  },
+};
