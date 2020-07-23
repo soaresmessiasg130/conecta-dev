4fbdbe11477d2570e926b8890416453f3eca817d
Messias Soares <soaresmessiasg130@gmail.com>
2020-07-22 23:56:05 -0300

    Adjusts

--- src/pages/Home/components/Header/index.js
+++ src/pages/Home/components/Header/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import {
   AppBar,
   Toolbar,
-  Box,
-  Popover
+  Box
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Account from './Account';
