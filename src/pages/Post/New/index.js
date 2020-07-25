5798d16a8c2b9ebc7cf209439f4d380b80e5e3e4
Messias Soares <soaresmessiasg130@gmail.com>
2020-07-25 17:19:15 -0300

    Editor de Post

--- src/pages/Post/New/index.js
+++ src/pages/Post/New/index.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  Button
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: 'red',
+    height: 'calc(100% - 70px)',
+    overflow: 'scroll'
+  },
+  appBar: {
+    top: 'auto',
+    bottom: '0',
+    alignItems: 'center'
+  },
+  button: {
+    marginRight: theme.spacing(2),
+  },
+}));
 
 function New() {
+  const styles = useStyles();
+
   return (
-    <h1>Post</h1>
-  );
+    <>
+      <Box display='flex' className={styles.root}>
+        <Box>Editor</Box>
+        <Box>Resultado</Box>
+      </Box>
+
+      <AppBar position='fixed' color='inherit' className={styles.appBar}>
+        <Toolbar>
+          <Button className={styles.button}>
+            Salvar Rascunho
+          </Button>
+
+          <Button color='secondary' variant='outlined' className={styles.button}>
+            Publicar
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </>
+);
 }
 
 export default New;
